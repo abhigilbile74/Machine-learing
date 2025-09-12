93ecff763d32bd74f78f2b6b8aa5137a0896d233
abhi <abhigilbile74@gmail.com>
2025-09-12 08:00:32 +0530

    login and expection

---
+++ src/logger.py
@@ -0,0 +1,20 @@
+import logging
+import os
+from datetime import datetime
+
+# Create logs directory
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+
+# Log file with timestamp
+log_file = os.path.join(log_dir, f"{datetime.now().strftime('%Y-%m-%d')}.log")
+
+# Configure logging
+logging.basicConfig(
+    filename=log_file,
+    level=logging.INFO,   # Levels: DEBUG, INFO, WARNING, ERROR, CRITICAL
+    format="%(asctime)s - %(levelname)s - %(filename)s - %(lineno)d - %(message)s",
+)
+
+
+
