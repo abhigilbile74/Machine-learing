aeb956a4ef72c4c1a85bcc1b59d6d540c36f40ba
abhi <abhigilbile74@gmail.com>
2025-09-12 22:40:53 +0530

    EDA and testing

--- src/logger.py
+++ src/logger.py
@@ -1,6 +1,7 @@
-import logging
+
 import os
 from datetime import datetime
+from src.logger import logging
 
 # Create logs directory
 log_dir = "logs"
@@ -15,6 +16,7 @@ logging.basicConfig(
     level=logging.INFO,   # Levels: DEBUG, INFO, WARNING, ERROR, CRITICAL
     format="%(asctime)s - %(levelname)s - %(filename)s - %(lineno)d - %(message)s",
 )
+
 
 
 
